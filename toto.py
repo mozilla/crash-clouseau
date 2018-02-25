725bc8d463f9bb627a509ef7ad69544f90b5c524
Calixte Denizet <calixte.denizet@gmail.com>
2018-02-25 16:06:14 +0100

    Remove useless files

--- toto.py
+++
@@ -1,8 +0,0 @@
-import json
-
-with open('/tmp/hg.log', 'r') as In:
-    data = json.load(In)
-
-new = [{'author': i['author']} for i in data]
-with open('/tmp/hg.json', 'w') as Out:
-    json.dump(new, Out)
